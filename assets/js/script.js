05e5d4333ad48bd95cc6435bbe959027bd474d14
aprashant21 <aprashant21@tbc.edu.np>
2023-01-05 23:23:09 +0545

    contact form added

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,20 @@ window.addEventListener("scroll", function () {
     : goTopBtn.classList.remove("active");
 });
 
+//submitForm function
+function submitForm() {
+  // Get the values of the form fields
+  var fullName = document.getElementById("fullName").value;
+  var email = document.getElementById("email").value;
+  var phoneNumber = document.getElementById("phoneNumber").value;
+  var message = document.getElementById("message").value;
+
+  // Check if all the required fields have been filled out
+  if (fullName && email && phoneNumber && message) {
+    // All the required fields have been filled out, so submit the form
+    alert("Thank you for your message! We will get back to you as soon as possible.");
+  } else {
+    // One or more of the required fields are empty, so show an error message
+    alert("Error: Please fill out all the required fields.");
+  }
+}
